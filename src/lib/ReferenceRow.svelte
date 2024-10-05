d84136ff0ec428ece1524ceb72c44ee890b8465d
Vojtěch divoký <divokyvojta@gmail.com>
2024-10-05 20:40:52 +0200

    studio reference pic #27

--- src/lib/ReferenceRow.svelte
+++ src/lib/ReferenceRow.svelte
@@ -1,14 +1,58 @@
 <script>
+	import { blur } from 'svelte/transition';
+
 	export let label, image;
+
+	let showImage = false;
+	let cursorX = 0;
+	let cursorY = 0;
+	let imageOffsetY = 20;
+	let imageHeight = 0;
+
+	function handleMouseMove(event) {
+		cursorX = event.clientX;
+		cursorY = event.clientY;
+
+		imageOffsetY = cursorY > window.innerHeight / 2 ? -imageHeight - 10 : 20;
+	}
+
+	const switchImageVisibility = (newVal) => (showImage = newVal);
+	const handleImageLoad = (event) => (imageHeight = event.target.offsetHeight);
 </script>
 
-<div>
-	<p>{label}<img src={image} alt="" /></p>
+<div class="fx-text">
+	<div
+		on:mouseenter={() => switchImageVisibility(true)}
+		on:mouseleave={() => switchImageVisibility(false)}
+		on:mousemove={handleMouseMove}
+	>
+		<p>{label}</p>
+	</div>
+	{#if showImage}
+		<img
+			src={image}
+			alt="reference Image"
+			class="hover-image {showImage ? 'visible' : ''}"
+			style="top: calc({cursorY}px + {imageOffsetY}px); left: {cursorX}px;"
+			on:load={(event) => handleImageLoad(event)}
+			transition:blur
+		/>
+	{/if}
+</div>
+
+<div class="col-text">
+	<p>{label}</p>
+	<img src={image} alt="reference Image" />
 </div>
 
 <style>
 	div {
 		margin-left: 38px;
+	}
+
+	.fx-text,
+	.col-text {
+		all: unset;
 	}
 
 	p {
@@ -24,28 +68,51 @@
 		transition: all 0.35s;
 	}
 
-	p:hover {
+	div:hover p {
 		color: #ffffff;
 		background-color: var(--pink);
 	}
 
-	p:hover img {
+	.hover-image {
+		position: fixed;
+		pointer-events: none;
+		transition: opacity 0.2s ease;
+		opacity: 0;
+
+		border-radius: var(--border-radius);
+		width: 50%;
+		height: auto;
+	}
+
+	.hover-image.visible {
 		opacity: 1;
 	}
 
-	img {
-		position: absolute;
-		right: 10%;
-		margin-top: 2.5%;
+	.col-text {
+		display: none;
+	}
 
-		max-width: 50%;
+	.col-text p{
+		width: 100%;
+		
+		padding-left: 0;
+		margin-top: 50px;
+		
+		text-align: center;
+	}
+
+	.col-text img{
+		width: 100%;
 		height: auto;
+	}
 
-		opacity: 0;
-		pointer-events: none;
+	@media screen and (max-width: 600px) {
+		.fx-text {
+			display: none;
+		}
 
-		border-radius: var(--border-radius);
-		transition: all 0.35s;
-		transition-delay: 0.2s;
+		.col-text {
+			display: block;
+		}
 	}
 </style>
