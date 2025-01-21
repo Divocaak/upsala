2c718c149699322f42b69b7867694bd0e6dcc406
Vojtěch divoký <divokyvojta@gmail.com>
2025-01-21 20:51:31 +0100

    vystavovani pacakge change

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        // adapter-node is used for production builds
+        adapter: adapter({
+            // default options are shown
+            out: 'build',
+            precompress: false,
+            envPrefix: '',
+            polyfill: true
+        })
+    },
+
+    // Enable vite preprocessing for components
+    preprocess: vitePreprocess()
 };
 
 export default config;
