6b395942fbd1aa3428fe500b489bea607c462a68
Vojtěch divoký <divokyvojta@gmail.com>
2025-11-06 19:28:26 +0100

    KONTAKT #83

---
+++ src/lib/containers/LandingText.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let text = 'lorem ipsum';
+</script>
+
+<p>{text}</p>
+
+<style>
+	p {
+		font-weight: 400;
+		font-size: var(--text-64);
+		line-height: 130%;
+		text-align: center;
+        
+        max-width: 60%;
+        margin: 0 auto;
+	}
+</style>
--- src/routes/(designed)/kontakt/+page.svelte
+++ src/routes/(designed)/kontakt/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-	import Contact from '$lib/Contact.svelte';
-	import LeadContainer from '$lib/containers/LeadContainer.svelte';
-	import StyledForm from '$lib/StyledForm.svelte';
 	import { onMount } from 'svelte';
+	import LeadContainer from '$lib/containers/LeadContainer.svelte';
+	import LandingText from '$lib/containers/LandingText.svelte';
+	import NewForm from '$lib/NewForm.svelte';
+	import RunningText from '../../../lib/RunningText.svelte';
 
 	let data = {};
 	onMount(async () => {
@@ -15,37 +16,80 @@
 	<title>kontakt</title>
 </svelte:head>
 
-<LeadContainer title="Kontakt" paddedTitle={true}>
-	<p class="contact" slot="l">
-		UPSALA, s.r.o. <br />
-		Jakubská 1, 602 00 Brno
-	</p>
-	<div class="contact-container" slot="r">
-		{#each data as contact}
-			<Contact name={contact.name} tel={contact.tel} mail={contact.mail} />
-		{/each}
+<LeadContainer title="Kontakt" paddedTitle={true} />
+<!-- TODO from json -->
+<LandingText
+	text="Vždy jsme připraveni vzít váš telefon, odpovědět na váš e-mail nebo vás přivítat u našich dveří."
+/>
+<div class="table">
+	<div>
+		<p class="name">Tomáš Zahradníček</p>
+		<a href="mailto:upsala@upsala.cz">upsala@upsala.cz</a><br />
+		<a href="tel:+420604924084">+420&nbsp;604&nbsp;924&nbsp;084</a>
+		<div class="invoice">
+			<p class="contact">
+				UPSALA,&nbsp;s.r.o.<br />
+				Jakubská&nbsp;1, 602&nbsp;00&nbsp;Brno
+			</p>
+			<p>
+				IČ:&nbsp;29301441<br />
+				DIČ:&nbsp;CZ29301441
+			</p>
+		</div>
 	</div>
-</LeadContainer>
-<!-- <StyledForm
-	label="Kontaktujte nás"
-	subject="Kontaktujte nás"
-	descriptionLabel="Zpráva"
-	isArch={false}
-/> -->
+	<div>
+		<p class="name">Máte dotaz?</p>
+		<NewForm />
+	</div>
+</div>
+<!-- TODO from json -->
+<RunningText text="# studio # kreativita # zkušenosti " />
 
 <style>
-	.contact {
-		padding-top: 100px;
-		font-size: var(--text-36);
-		width: 60%;
-	}
-
-	.contact-container {
-		display: flex;
-		flex-wrap: wrap;
-
-		justify-content: center;
+	.table,
+	.invoice {
 		position: relative;
 		width: 100%;
+		height: 100%;
+
+		display: flex;
+		flex-direction: row;
+	}
+
+	.table {
+		padding: 0 var(--general-px);
+		width: calc(100% - 2 * var(--general-px));
+
+		margin-top: 10rem;
+	}
+
+	.table div,
+	.invoice p {
+		position: relative;
+		min-width: 50%;
+	}
+
+	.name,
+	.invoice p {
+		font-weight: 400;
+		font-size: var(--text-20);
+	}
+
+	a {
+		font-weight: 400;
+		font-size: var(--text-36);
+
+		color: var(--black);
+		transition: all var(--link-transition);
+	}
+
+	a:hover {
+		color: var(--pink);
+	}
+
+	@media screen and (max-width: 900px) {
+		.table {
+			flex-direction: column;
+		}
 	}
 </style>
