1e13792dab9f52334c482c82af0d38c797e7277b
Vojtěch divoký <divokyvojta@gmail.com>
2025-11-25 13:10:32 +0100

    reference links #92

--- src/routes/api/save-reference/+server.js
+++ src/routes/api/save-reference/+server.js
@@ -1,14 +1,9 @@
 import { saveJson } from '$lib/admin/scripts/saveJson.js';
-import { processImage } from '$lib/admin/scripts/saveImages.js';
 
 export async function POST({ request }) {
 	const data = await request.json();
 
 	let errs = [];
-	for (const reference of data.reference) {
-		reference.image = (await processImage(reference.image, 'reference/', errs)) ?? reference.image;
-	}
-
 	// error handling outside of for loop
 	// code does not crash under first error but continues with the rest
 	const jsonResponse = await saveJson(data, './dynamic/jsons/data/reference.json');
